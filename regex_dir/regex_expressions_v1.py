0f2d28115d7aaefd94addaa2ead75afa8bba9e8a
hurtuk18 <adam.hurtuk@gmail.com>
2022-11-24 15:03:05 +0100

    New stage of project

--- regex_dir/regex_expressions_v1.py
+++ regex_dir/regex_expressions_v1.py
@@ -1,19 +1,19 @@
 import re
 
-'''
+"""
     This file contains all regex expressions which I'll use in my Parser
     
     author: Bc. Adam Hurtuk
-'''
+"""
 
 
-'''
+"""
     Infobox book section
-'''
+"""
 
 
 def regex_plain(input: str) -> list:
-    regex_output = re.findall(r'(?<=\= )(.*)(?=\\n)', input)
+    regex_output = re.findall(r"(?<=\= )(.*)(?=\\n)", input)
     if regex_output:
         return regex_output
     else:
@@ -21,8 +21,8 @@ def regex_plain(input: str) -> list:
 
 
 def regex_name(input: str) -> list:
-    if '&lt' in input:
-        regex_output = re.findall(r'(?<=\= )(.*)(?=\&lt)', input)
+    if '<' in input:
+        regex_output = re.findall(r"(?<=\= )(.*)(?=\<)", input)
         if regex_output:
             return regex_output
         else:
@@ -32,7 +32,7 @@ def regex_name(input: str) -> list:
 
 
 def regex_pub(input: str) -> list:
-    regex_output = re.findall(r'(\d{4})(.*?)', input)
+    regex_output = re.findall(r"(\d{4})(.*?)", input)
     if regex_output:
         return regex_output[0][0]
     else:
@@ -40,7 +40,7 @@ def regex_pub(input: str) -> list:
 
 
 def regex_pages(input: str) -> list:
-    regex_output = re.findall(r'(\d{1,4})(.*?)', input)
+    regex_output = re.findall("(\d{1,4})(.*?)", input)
     if regex_output:
         return regex_output[0][0]
     else:
@@ -49,7 +49,7 @@ def regex_pages(input: str) -> list:
 
 def regex_other(input: str) -> list:
     if '[' in input:
-        regex_output = re.findall(r'(?<=\[\[)(.*?)(?=[\||\]])', input)
+        regex_output = re.findall(r"(?<=\[\[)(.*?)(?=[\||\]])", input)
         if regex_output:
             return regex_output
         else:
@@ -58,6 +58,6 @@ def regex_other(input: str) -> list:
         return regex_plain(input)
 
 
-'''
+"""
     End of Infobox book section
-'''
+"""
